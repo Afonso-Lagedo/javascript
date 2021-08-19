cdeee2799c0ffe57b09257675e35b7202e5737b1
Afonso-cmd <afonso.ur@gmail.com>
2021-08-19 18:41:48 -0300

    Enhanced Object Literals

--- js/main.js
+++ js/main.js
@@ -1,63 +1,67 @@
-/*function produto(a,b){
-	return a* b;
-}
-console.log(produto(2,3));
-console.log(produto(2));//not a number, undefined 
+//old mode creat object
+/*var obj ={
+	prop1: 'Afonso'
+};
+console.log(obj);
 */
 
-/*function produto(a,b){
-	b= b || 1;// if B is false, b =1
-	return a*b;
+//propety out function
+/*
+var prop1 = "Afonso";
+var obj = {
+	prop1: prop1
+};
+console.log(obj);*/
+
+
+//with ECMA6
+//if name equal
+/*var prop1 = 'Afonso';
+var obj = {
+	prop1
+};
+console.log(obj);*/
+
+
+//for functions
+/*function method1(){
+	console.log("method1 is on");
 }
-console.log(produto(2));
-console.log(produto(5,0));// 0 is false value in javascript, b = 1
+var obj = {
+	method1
+};
+obj.method1();
 */
 
-//idem up, version two
 
-/*function produto(a,b){
-	b = typeof b === 'undefined' ? 1 : b;//verification of type, if undefined, b=1;
-	return a*b;
-}
-console.log(produto(2,0));
-console.log(produto(2)); 
-*/
+//same function up
 
-//idem up, version three with conditional
-
-/*function produto(a,b){
-	if(b = typeof b === 'undefined') {;//verification of type, if undefined, b=1;
-		b=1;
+/*var obj = {
+	sum: function sum(a,b) {
+		return a+b;
 	}
-	return a*b;
-}
-console.log(produto(2,0));
-console.log(produto(2)); 
+};
+console.log(obj.sum(1,5));
+*/
+//sume function up, with ECMA 6
+var obj = {
+	sum(a,b) {//method pointing function
+		return a+b;
+	}
+};
+
+console.log(obj.sum(1,5));
+
+//old mode of  propety  acess
+/*var propName = 'Afonso';
+var obj = {};
+obj[propName+'teste concatenação'] = 'Afonso teste';
+console.log(obj);
 */
 
-//idem up, version four with new declaration default
-
-/*function produto(a,b=1){//default value is one
-	return a*b;
-}
-console.log(produto(2,0));
-console.log(produto(2)); 
-*/
-
-//idem up, version four with new declaration default
-/*function produto(a,b=a){//for exemple (b=a, a) is error, variable a is not declareted
-	return a*b;
-}
-console.log(produto(2,0));
-console.log(produto(2)); 
-*/
-
-//lazy evaluation
-function numeroAleatorio (){//ever when acionatied will genereted new random number 
-	return Math.random()*10;
-}
-
-function produto(a,b=numeroAleatorio()){//for exemple (b=a, a) is error, variable a is not declareted
-	return a*b;
-}
-console.log(produto(2));
+//same function up with ECMA6
+var propName = 'Afonso';
+var obj = {
+	[propName+'teste concatenação'] : 'Afonso teste'
+};
+console.log(obj);
