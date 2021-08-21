0574bb0b481c7e520713abaf4ebb521a11e43416
Afonso-cmd <afonso.ur@gmail.com>
2021-08-21 19:10:04 -0300

    Symbols e Iterators

--- js/main.js
+++ js/main.js
@@ -1,139 +1,82 @@
-//Destructuring Assignment
+//symbols (identifier)
 
 /*
-var arr = ['corsa', 'fusca', 'mustang'];
-//old mode of passing values for variables of old mode
-var car1 = arr[0];
-var car2 = arr[1];
-var car3 = arr[2];
+const identificador = Symbol();
+console.log(identificador);
 
-console.log(arr);
-console.log(car1+car2+car3);
+//passing value, only identification
 
-*/
-// using destructuring
-/*
-var [car1, car2, car3] = ['corsa ', 'uno ', 'camaro'];
+const id = Symbol('test');
+console.log(id);
 
-console.log(car1+car2+car3);
+//test showing what one Symbol isn't equal a other Symbol
+const id1 = Symbol('test1');
+const id2 = Symbol('test1');
+console.log(id1 === id2); // is false
 */
 
 
-
-//array inside array
 /*
-var arr=['mustang', ['camaro']];
-var car =arr[1][0];
-console.log (car); 
+const id = Symbol('test');
 
-var [car1, [car2]] = ['mustang ', ['camaro']];
-console.log(car1,car2);
-*/
-
-
-
-/*
-//in Object
-var obj = {
-	name: 'Afonso'
-}
-var nome = obj.name;
-
-console.log(nome);
-//Alert: Destructuring not creat a object or array
-//Same example up with Destructuring
-var {name} = obj;//alert: propety name equal a variable name 
-console.log(name);
-
-var {name: name2} = obj;//destructuring name variable  and playing inside name2 variable
-//not altered the object
-console.log(name2);
-*/
-
-
-
-//with object inside object
-//old mode
-
-/*var obj = {
-	name:'Afonso',
-	props: {
-		age:26
-	}
+const obj = {
+	[id]: 'propety value'
 };
-*/
 
-/*var teste = obj.props.age;
-console.log(teste);*/
-
-//with destructuring
-/*var {
- 	props: {age : age2}  
- } = obj;
-console.log(age2);
-*/
-
-//same function up, with objects and vectors
-/*
-var obj ={
-	nome: 'Afonso',
-	props:{
-		age: 27,
-		time:['Corinthians', '49ers']
-	}
- }
-
-var {
-	props: {
-		age : age2, 
-		time: [soc, fa]}
-} = obj;
-
-//old mode; var soc = obj.props.time[0];
-
-console.log(fa);
+console.log(obj);
+//alert
+console.log(Object.keys(obj));// return attributes of objects
+console.log(Object.getOwnPropertySymbols(obj));// return attributes of Symbol
 */
 
 
+//well know symbols
 
-//example2
-/*
-var arr = [{name:'Afonso', age:27}];
-//OLD MODE var pessoa = arr[0].name;
+const arr = [1,2,3,4];
 
-var [{name}]=arr;
-console.log(name);
+const it = arr[Symbol.iterator]();
 
-//or
-var [{name: firstName}]=arr;
-console.log(firstName);
-*/
-
-
-/*destructuring it work with VAR LET CONST*/
-
-//in functions
-//old mode
-function sum(arr){
-	return arr[0]+arr[1];
+for (let i in arr){//para cada elemento me traga a posição
+	console.log(it.next());
 }
+console.log(it.next());//finished
 
-console.log(sum([5,5]));
-
-//with destructuring
-function sum2([a,b] = []){
-	return a+b;
+//with String
+const str = 'Afonso';
+for (let value of str){
+	console.log(value);
 }
-console.log([2,4]);
-
-//with default values
-function sum3([a,b]=[1,0]){
-	return a+b;
-}
-console.log(sum3());
 
 //with object
-function sum4({a, b}){
-	return a+b;
-}
-console.log(sum4({a:4, b:5}));
+
+const obj ={
+	values: [1,2,3,4],
+	[Symbol.iterator](){
+		let i = 0;
+		return{
+			next: () => {
+				i++;
+
+				return{
+					value: this.values[i -1],
+					done: i > this.values.length 
+				};
+			}
+		};
+	}
+};
+
+const teste = obj[Symbol.iterator]();
+
+console.log(teste.next());
+console.log(teste.next());
+console.log(teste.next());
+console.log(teste.next());
+console.log(teste.next());
+
+for (let valor of obj){
+	console.log(valor);
+} 
+
+const arr2 = [...obj];
+console.log(arr2);
