dc0025afc305f542f6dc4577a6e46059fe130707
Afonso-cmd <afonso.ur@gmail.com>
2021-08-21 15:29:03 -0300

    Destructuring em ReactJS

--- js/main.js
+++ js/main.js
@@ -1,148 +1,139 @@
-/*function soma(a,b) {
+//Destructuring Assignment
+
+/*
+var arr = ['corsa', 'fusca', 'mustang'];
+//old mode of passing values for variables of old mode
+var car1 = arr[0];
+var car2 = arr[1];
+var car3 = arr[2];
+
+console.log(arr);
+console.log(car1+car2+car3);
+
+*/
+// using destructuring
+/*
+var [car1, car2, car3] = ['corsa ', 'uno ', 'camaro'];
+
+console.log(car1+car2+car3);
+*/
+
+
+
+//array inside array
+/*
+var arr=['mustang', ['camaro']];
+var car =arr[1][0];
+console.log (car); 
+
+var [car1, [car2]] = ['mustang ', ['camaro']];
+console.log(car1,car2);
+*/
+
+
+
+/*
+//in Object
+var obj = {
+	name: 'Afonso'
+}
+var nome = obj.name;
+
+console.log(nome);
+//Alert: Destructuring not creat a object or array
+//Same example up with Destructuring
+var {name} = obj;//alert: propety name equal a variable name 
+console.log(name);
+
+var {name: name2} = obj;//destructuring name variable  and playing inside name2 variable
+//not altered the object
+console.log(name2);
+*/
+
+
+
+//with object inside object
+//old mode
+
+/*var obj = {
+	name:'Afonso',
+	props: {
+		age:26
+	}
+};
+*/
+
+/*var teste = obj.props.age;
+console.log(teste);*/
+
+//with destructuring
+/*var {
+ 	props: {age : age2}  
+ } = obj;
+console.log(age2);
+*/
+
+//same function up, with objects and vectors
+/*
+var obj ={
+	nome: 'Afonso',
+	props:{
+		age: 27,
+		time:['Corinthians', '49ers']
+	}
+ }
+
+var {
+	props: {
+		age : age2, 
+		time: [soc, fa]}
+} = obj;
+
+//old mode; var soc = obj.props.time[0];
+
+console.log(fa);
+*/
+
+
+
+//example2
+/*
+var arr = [{name:'Afonso', age:27}];
+//OLD MODE var pessoa = arr[0].name;
+
+var [{name}]=arr;
+console.log(name);
+
+//or
+var [{name: firstName}]=arr;
+console.log(firstName);
+*/
+
+
+/*destructuring it work with VAR LET CONST*/
+
+//in functions
+//old mode
+function sum(arr){
+	return arr[0]+arr[1];
+}
+
+console.log(sum([5,5]));
+
+//with destructuring
+function sum2([a,b] = []){
 	return a+b;
 }
+console.log([2,4]);
 
-console.log(soma(5,2));
-*/
-
-//unlimited arguments for same function up
-/*function soma(a,b){
-	var valor = 0;
-
-	for (var i=0; i < arguments.length; i++){
-		valor += arguments[i];
-	}
-
-	return valor;
+//with default values
+function sum3([a,b]=[1,0]){
+	return a+b;
 }
+console.log(sum3());
 
-console.log(soma(1,2,3,4));
-*/
-
-
-
-//same function up with rest operator ...      prototype of arguments is object, prototype of reset is array
-/*function soma(...elementos){
-	return elementos.reduce((a,b) => a+b,0);//reduce: execut a function defined for you for each element of vector, in case a+b and position initial, in case 0
+//with object
+function sum4({a, b}){
+	return a+b;
 }
-console.log(soma(1,2,3,4));
-*/
-
-
-//get the rest function
-/*function teste(a,b,...resto){
-	console.log(a,b,resto);
-}
-console.log(teste(1,2,3,4));//a, b and rest
-*/
-
-
-
-/*
-const soma = (...argumentos) => argumentos.reduce((a,b)=> a+b, 1);//initial value is one 
-
-const teste =(...arg) => {
-	return soma.apply(undefined, arg);// passing of function soma for function teste
-};
-
-console.log(teste(1,2,3,4,5));
-*/
-
-//same function up with spread operator "break arrguments"
-/*const soma = (...argumentos) => argumentos.reduce((a,b)=> a+b, 2);//initial value is two
-const teste =(...arg) => {
-	return soma(...arg);// passing of function soma for function teste
-};
-
-console.log(teste(1,2,3,4,5));
-*/
-
-
-//spread operator in strings
-/*const str = 'Afonso Silva';
-
-function teste(...argumentos) {
-	console.log(argumentos);
-}
-
-teste(...str);// passing arguments, obs.: String is character join 
-*/
-
-
-//spread operator in arrays
-const arr = [1,2,3,4];
-
-function teste (){
-	console.log(arguments);//for get all arguments 
-}
-
-teste(...arr);  
-
-//constructing array with spread 
-//old array join
-/*
-const arr2 = arr.concat([5,6,7,8]);
-
-console.log(arr2);
-*/
-
-
-//same function up with spread operator
-const arr2 = [...arr, 5,6];
-console.log(arr2);
-
-const arr3 = [...arr2, ...arr, 0, 0];
-console.log(arr3);
-
-const arrClone = [...arr];
-console.log(arrClone);
-
-//creating of objects iteraveis with spread operator
-
-const obj ={
-	atributo1:123
-};
-
-const obj2 ={
-	...obj,
-	atributo2: 'teste'
-};
-
-
-//obs.: it is error: const obj2=[...obj]; // in case obj2 isn't iteravel
-console.log(obj2);
-
-//overwrite
-const objeto={
-	a:123
-};
-const objeto2={
-	...objeto,
-	a:'Afonso teste'
-};
-
-console.log(objeto2);
-
-//clone
-/*
-const a ={
-	t:123
-}
-
-const b=a;
-
-b.t=456;
-console.log(a);//overwrite propety 
-console.log(b);
-*/
-const a ={
-	t:123
-}
-
-const b ={
-	...a
-}
-
-b.t=456;
-console.log(a);//not overwrite propety
-console.log(b);
+console.log(sum4({a:4, b:5}));
