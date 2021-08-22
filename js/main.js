d686d1a229d3eee785b6943d686576ed3ba59e6a
Afonso-cmd <afonso.ur@gmail.com>
2021-08-22 10:13:18 -0300

    Generators

--- js/main.js
+++ js/main.js
@@ -1,82 +1,84 @@
-//symbols (identifier)
+//iterator is a interface
+//Generators is function with pause
 
 /*
-const identificador = Symbol();
-console.log(identificador);
+function teste() {
+	console.log('Hello');
 
-//passing value, only identification
+	console.log('from');
+	
+	console.log('function');
+}
 
-const id = Symbol('test');
-console.log(id);
+teste();
+*/
 
-//test showing what one Symbol isn't equal a other Symbol
-const id1 = Symbol('test1');
-const id2 = Symbol('test1');
-console.log(id1 === id2); // is false
+//same function, with pause
+/*function* teste() {//* is function with pause
+	console.log('Hello');
+	yield; //reserve word for pause
+	console.log('from');
+	yield 22; //passing value for function. If not passing value undefined
+	console.log('function');
+	yield;
+}
+
+const it = teste();
+//tests of excutation
+console.log(it);
+
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+*/
+
+//same function, with passing values for function 
+/*function* teste() {
+	console.log('Hello');
+	yield; 
+	console.log('from');
+	const valor = yield 33; //when
+	console.log(valor);
+}
+
+const it = teste();
+//tests of excutation
+console.log(it);
+
+console.log(it.next());
+console.log(it.next());
+console.log(it.next('Test: passing value'));
+*/
+
+//exemple two
+/*function* naturais (){
+	let numero = 0;
+	while(true){
+		yield numero;
+		numero++;
+	}
+}
+
+const a = naturais();
+
+console.log(a.next());
+console.log(a.next());
+console.log(a.next());
+console.log(a.next());
 */
 
 
-/*
-const id = Symbol('test');
-
-const obj = {
-	[id]: 'propety value'
-};
-
-console.log(obj);
-//alert
-console.log(Object.keys(obj));// return attributes of objects
-console.log(Object.getOwnPropertySymbols(obj));// return attributes of Symbol
-*/
-
-
-//well know symbols
-
-const arr = [1,2,3,4];
-
-const it = arr[Symbol.iterator]();
-
-for (let i in arr){//para cada elemento me traga a posição
-	console.log(it.next());
-}
-console.log(it.next());//finished
-
-//with String
-const str = 'Afonso';
-for (let value of str){
-	console.log(value);
-}
-
-//with object
-
+//and now with object
 const obj ={
-	values: [1,2,3,4],
-	[Symbol.iterator](){
-		let i = 0;
-		return{
-			next: () => {
-				i++;
-
-				return{
-					value: this.values[i -1],
-					done: i > this.values.length 
-				};
-			}
-		};
+	valores:[1,2,3,4],
+	*[Symbol.iterator]() {
+		for (var i = 0; i< this.valores.length; i++){
+			yield this.valores[i];
+		}
 	}
 };
 
-const teste = obj[Symbol.iterator]();
-
-console.log(teste.next());
-console.log(teste.next());
-console.log(teste.next());
-console.log(teste.next());
-console.log(teste.next());
-
-for (let valor of obj){
+for (let valor of obj){//for each element of obj...
 	console.log(valor);
-} 
+}
 
-const arr2 = [...obj];
-console.log(arr2);
